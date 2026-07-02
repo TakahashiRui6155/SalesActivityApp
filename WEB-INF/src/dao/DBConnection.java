16eb69c2d39f5bfc9288ec1cc4d7c2f579a767d4
高橋　琉維 <@DESKTOP-FG39OFO>
2026-07-02 18:04:02 +0900

    JDBC接続クラスを作成

--- WEB-INF/src/dao/DBConnection.java
+++ WEB-INF/src/dao/DBConnection.java
@@ -1,5 +1,16 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
 
+    private static final String URL = "jdbc:mariadb://localhost:3306/sales_activity_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("org.mariadb.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
