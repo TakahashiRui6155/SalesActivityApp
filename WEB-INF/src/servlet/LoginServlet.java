8b2357f7d0a97d6e1850af7c082ef7b3fb89c9b6
高橋　琉維 <@DESKTOP-FG39OFO>
2026-07-09 17:59:34 +0900

    エラー修正

--- WEB-INF/src/servlet/LoginServlet.java
+++ WEB-INF/src/servlet/LoginServlet.java
@@ -16,6 +16,12 @@ import model.User;
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/jsp/login.jsp")
+               .forward(request, response);
+    }
+   
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
